a53339f55542176c8c38e948d4bb59769267928f
camalow <62999432+camalow@users.noreply.github.com>
2022-02-23 17:28:54 +0500

    M

--- pages/projects.vue
+++ pages/projects.vue
@@ -1,5 +1,76 @@
 <template>
-  <div class="projects">Projects page</div>
+  <div id="projects-root">
+    <div class="container">
+      <h2 class="#cloud-title-text">Cloud-ultrasound</h2>
+      <div id="cloud-image">
+        <img
+          width="80%"
+          class="img-fluid"
+          src="../static/images/cloud.png"
+          alt="cloud image"
+        />
+      </div>
+      <div class="row">
+        <div class="col-sm-8 border-right">
+          <h3 class="about-title">About</h3>
+          <p class="about-text">
+            True Cloud Web Hosting All of the hosting packages we offer are
+            deployed instantly on our SSD powered cloud. We don't use dedicated
+            servers that operate on single pieces of hardware. True Cloud Web
+            Hosting All of the hosting packages we offer are deployed instantly
+            on our SSD powered cloud. We don't use dedicated servers that
+            operate on single pieces of hardware.
+          </p>
+        </div>
+        <div class="col-sm-4">
+          <h3 class="about-title">About</h3>
+          <p class="about-text">
+            True Cloud Web Hosting All of the hosting packages we offer are
+            deployed instantly on our SSD powered cloud.
+          </p>
+          <h3 class="about-title">About</h3>
+          <p class="about-text">
+            True Cloud Web Hosting All of the hosting packages we offer are
+            deployed instantly on our SSD powered cloud.
+          </p>
+        </div>
+      </div>
+      <div class="row mt-5">
+        <div class="col-4">
+          <h3 class="about-title">About</h3>
+        </div>
+        <div class="col-sm-8">
+          <p class="about-text">
+            True Cloud Web Hosting All of the hosting packages we offer are
+            deployed instantly on our SSD powered cloud. We don't use dedicated
+            servers that operate on single pieces of hardware. True Cloud Web
+            Hosting All of the hosting packages we offer are deployed instantly
+            on our SSD powered cloud. We don't use dedicated servers that
+            operate on single pieces of hardware.
+          </p>
+        </div>
+      </div>
+      <div class="row mt-2">
+        <div class="col-4">
+          <h3 class="about-title">+1.200</h3>
+          <p>users</p>
+        </div>
+        <div class="col-4">
+          <h3 class="about-title">+1.200</h3>
+          <p>users</p>
+        </div>
+        <div class="col-4">
+          <h3 class="about-title">+1.200</h3>
+          <p>users</p>
+        </div>
+      </div>
+      <br />
+      <h2 class="#cloud-title-text">Children robot lego</h2>
+      <div id="robot-image">
+        <img width="100%" src="../static/images/robot.png" alt="robot image" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
